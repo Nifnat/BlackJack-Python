f5cae3289cabde666b22a70b8daf664f5e33edec
Nifnat <nifnat@gmail.com>
2021-10-30 19:03:35 +0100

    Handle ace highs/lows.

--- hand.py
+++ hand.py
@@ -21,3 +21,37 @@ class Hand():
         for x in range(hand_size):
             hand.append(deck.pop(0))  # 0 is top card on deck
         return hand
+
+    def get_total_of_hand_ace_highest_if_possible(self, hand):
+        total = 0
+        total = self.get_total_of_hand_ace_high(hand)
+        if total > 21:
+            total = self.get_total_of_hand_ace_low(hand)
+            return total
+        return total
+
+    def get_total_of_hand_ace_low(self, hand):
+        total = 0
+        card_value = 0
+        for card in hand:
+            if card[0][0] == "queen" or card[0][0] == "king" or card[0][0] == "jack":
+                card_value = 10
+            elif card[0][0] == "ace":
+                card_value = 1
+            else:
+                card_value = card[0][2]
+            total = total + card_value
+        return total
+
+    def get_total_of_hand_ace_high(self, hand):
+        total = 0
+        card_value = 0
+        for card in hand:
+            if card[0][0] == "queen" or card[0][0] == "king" or card[0][0] == "jack":
+                card_value = 10
+            elif card[0][0] == "ace":
+                card_value = 11
+            else:
+                card_value = card[0][2]
+            total = total + card_value
+        return total
